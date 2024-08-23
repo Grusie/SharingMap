276d8673b2ff1b3af6c814237ee6e766b703af07
로피 <50761690+Choisehyeon@users.noreply.github.com>
2024-08-23 20:47:02 +0900

    feat: 디자인시스템 추가 (#8)
    * feat: Color 추가
    
    * feat: Font 추가
    
    * feat: Theme 추가
    
    * chore: ktlint 수정
    
    * refactor: 사용하지 않는 색상 제거

---
+++ app/src/main/java/com/grusie/sharingmap/designsystem/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.grusie.sharingmap.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF458FFF)
+val Red = Color(0xFFF41E00)
+val Black = Color(0xFF080809)
+val White = Color(0xFFFFFFFF)
+
+val Gray100 = Color(0xFFF1F4F7)
+val Gray200 = Color(0xFFE8EAEB)
+val Gray300 = Color(0xFFBABBBC)
+val Gray400 = Color(0xFF9A9C9F)
