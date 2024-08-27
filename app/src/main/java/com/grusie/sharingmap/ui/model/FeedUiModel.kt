af9f5750d4de8d80de5c78cb0169bcace6e16b36
Choisehyeon <chltpgus6275@gmail.com>
2024-08-27 19:23:28 +0900

    refactor: UserUiModel 넣어서 수정

--- app/src/main/java/com/grusie/sharingmap/ui/model/FeedUiModel.kt
+++ app/src/main/java/com/grusie/sharingmap/ui/model/FeedUiModel.kt
@@ -1,8 +1,7 @@
 package com.grusie.sharingmap.ui.model
 
 data class FeedUiModel(
-    val profileImage: String,
-    val title: String,
+    val user: UserUiModel,
     val date: String,
     val content: String,
     val contentImages: List<String>,
