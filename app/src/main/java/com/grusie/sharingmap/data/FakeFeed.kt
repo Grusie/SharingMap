1a532d2f851fd8113834971e83a7b750efbf80ae
Choisehyeon <chltpgus6275@gmail.com>
2024-08-29 23:23:50 +0900

    refactor: FeedUiModel 변수 추가

--- app/src/main/java/com/grusie/sharingmap/data/FakeFeed.kt
+++ app/src/main/java/com/grusie/sharingmap/data/FakeFeed.kt
@@ -1,9 +1,11 @@
 package com.grusie.sharingmap.data
 
+import com.grusie.sharingmap.ui.model.CommentUiModel
 import com.grusie.sharingmap.ui.model.FeedInfoUiModel
 import com.grusie.sharingmap.ui.model.FeedUiModel
 import com.grusie.sharingmap.ui.model.LocationUiModel
 import com.grusie.sharingmap.ui.model.UserUiModel
+import java.time.LocalDate
 
 val fakeFeeds =
     listOf(
@@ -17,23 +19,60 @@ val fakeFeeds =
                 ),
             date = "2024.04.17",
             content = "honestatis",
+            archivings =
+                listOf(
+                    UserUiModel(
+                        id = 2,
+                        profileImage = "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
+                        name = "장원철",
+                    ),
+                    UserUiModel(
+                        id = 3,
+                        profileImage = "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
+                        name = "장원철",
+                    ),
+                ),
             contentImages =
                 listOf(
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                 ),
-            locationUiModel =
+            location =
                 LocationUiModel(
                     name = "Lilian Douglas",
                     address = "taciti",
                 ),
-            feedInfoUiModel =
+            feedInfo =
                 FeedInfoUiModel(
                     likeCount = 2617,
                     chatCount = 1606,
                     shareCount = 8829,
                 ),
+            comments = listOf(
+                CommentUiModel(
+                    id = 1,
+                    user =
+                    UserUiModel(
+                        id = 1,
+                        profileImage = "",
+                        name = "김민수",
+                    ),
+                    content = "안녕하세요",
+                    date = LocalDate.now(),
+                ),
+                CommentUiModel(
+                    id = 2,
+                    user =
+                    UserUiModel(
+                        id = 1,
+                        profileImage = "",
+                        name = "김민수",
+                    ),
+                    content = "안녕하세요",
+                    date = LocalDate.now(),
+                ),
+            ),
         ),
         FeedUiModel(
             id = 2,
@@ -51,12 +90,12 @@ val fakeFeeds =
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                 ),
-            locationUiModel =
+            location =
                 LocationUiModel(
                     name = "Lilian Douglas",
                     address = "taciti",
                 ),
-            feedInfoUiModel =
+            feedInfo =
                 FeedInfoUiModel(
                     likeCount = 2617,
                     chatCount = 1606,
@@ -79,12 +118,12 @@ val fakeFeeds =
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                     "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000",
                 ),
-            locationUiModel =
+            location =
                 LocationUiModel(
                     name = "Lilian Douglas",
                     address = "taciti",
                 ),
-            feedInfoUiModel =
+            feedInfo =
                 FeedInfoUiModel(
                     likeCount = 2617,
                     chatCount = 1606,
--- app/src/main/java/com/grusie/sharingmap/ui/model/FeedUiModel.kt
+++ app/src/main/java/com/grusie/sharingmap/ui/model/FeedUiModel.kt
@@ -5,7 +5,9 @@ data class FeedUiModel(
     val user: UserUiModel,
     val date: String,
     val content: String,
+    val archivings: List<UserUiModel> = emptyList(),
     val contentImages: List<String>,
-    val locationUiModel: LocationUiModel,
-    val feedInfoUiModel: FeedInfoUiModel,
+    val location: LocationUiModel,
+    val feedInfo: FeedInfoUiModel,
+    val comments: List<CommentUiModel> = emptyList(),
 )
