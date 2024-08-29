88ec811b87ffe1aaad6ab76455e2531e31eee4da
Choisehyeon <chltpgus6275@gmail.com>
2024-08-29 23:23:10 +0900

    refactor: 아카이빙 리스트 컴포넌트 수정 - LazyColumn 사이즈 지정 - image contentscale 타입 지정

--- app/src/main/java/com/grusie/sharingmap/designsystem/component/User.kt
+++ app/src/main/java/com/grusie/sharingmap/designsystem/component/User.kt
@@ -2,6 +2,7 @@ package com.grusie.sharingmap.designsystem.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -26,7 +28,7 @@ fun UserLazyColumn(
     users: List<UserUiModel>,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+    LazyColumn(modifier = Modifier.fillMaxWidth().fillMaxHeight(0.6f)) {
         itemsIndexed(users) { index, user ->
             UserItem(user)
         }
@@ -42,6 +44,7 @@ fun UserItem(
         AsyncImage(
             model = user.profileImage,
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier =
                 Modifier
                     .size(32.dp)
