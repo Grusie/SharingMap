20a4357e83e394fb037c951493a03e8e01146655
Choisehyeon <chltpgus6275@gmail.com>
2024-09-02 14:25:04 +0900

    refactor: pagerState를 사용하지 않도록 CustomTab 컴포넌트 수정

--- app/src/main/java/com/grusie/sharingmap/designsystem/component/CustomTab.kt
+++ app/src/main/java/com/grusie/sharingmap/designsystem/component/CustomTab.kt
@@ -1,9 +1,8 @@
 package com.grusie.sharingmap.designsystem.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -11,36 +10,36 @@ import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.grusie.sharingmap.designsystem.theme.Black
 import com.grusie.sharingmap.designsystem.theme.Typography
 import com.grusie.sharingmap.ui.model.SearchTab
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CustomTab(pagerState: PagerState, tabs: List<String>, modifier: Modifier = Modifier) {
-    val coroutineScope = rememberCoroutineScope()
-
+fun CustomTab(
+    selectedTabIndex: Int,
+    onClick: (Int) -> Unit,
+    tabs: List<String>,
+    modifier: Modifier = Modifier
+) {
     TabRow(
-        selectedTabIndex = pagerState.currentPage,
+        selectedTabIndex = selectedTabIndex,
         indicator = { tabPositions ->
             TabRowDefaults.SecondaryIndicator(
-                modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
                 color = Black,
                 height = 1.dp
             )
         }
     ) {
         tabs.forEachIndexed { index, value ->
-            Tab(selected = pagerState.currentPage == index,
+            Tab(
+                selected = selectedTabIndex == index,
                 onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
+                    onClick(index)
                 }
             ) {
                 Text(
@@ -60,11 +59,8 @@ fun CustomTab(pagerState: PagerState, tabs: List<String>, modifier: Modifier = M
 @Preview
 @Composable
 private fun CustomTabPreview() {
-    val pagerState = rememberPagerState(
-        pageCount = { SearchTab.entries.size },
-        initialPage = 0,
-        initialPageOffsetFraction = 0f
-    )
-    CustomTab(pagerState = pagerState, tabs = SearchTab.entries.map { it.title })
+    Column {
+        CustomTab(selectedTabIndex = 0, onClick = {}, tabs = SearchTab.entries.map { it.title })
+    }
 
 }
