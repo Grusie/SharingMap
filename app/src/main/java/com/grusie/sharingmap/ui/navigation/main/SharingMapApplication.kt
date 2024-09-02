6183ef52a03e0298fdc770afdfc1b3147351d9e7
Grusie <rbtjr02@naver.com>
2024-09-02 21:10:45 +0900

    fix: 힐트 적용 에러 수정

---
+++ app/src/main/java/com/grusie/sharingmap/ui/navigation/main/SharingMapApplication.kt
@@ -0,0 +1,8 @@
+package com.grusie.sharingmap.ui.navigation.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SharingMapApplication: Application() {
+}
