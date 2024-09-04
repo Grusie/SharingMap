d993c056c532b394233c22f8ec9ce332edcbf3b2
로피 <50761690+Choisehyeon@users.noreply.github.com>
2024-09-04 17:33:47 +0900

    fix: Application 파일이 겹처서 생기는 오류 수정 (#24)

--- app/src/main/java/com/grusie/sharingmap/ui/navigation/main/SharingMapApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.grusie.sharingmap.ui.navigation.main
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SharingMapApplication: Application() {
-}
