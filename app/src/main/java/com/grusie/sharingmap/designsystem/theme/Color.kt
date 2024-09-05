6396fd41ed9f12c0cc4f1250354631888be97584
Choisehyeon <chltpgus6275@gmail.com>
2024-09-05 13:37:03 +0900

    feat: UserInfo 공통 컴포넌트 구현

--- app/src/main/java/com/grusie/sharingmap/designsystem/theme/Color.kt
+++ app/src/main/java/com/grusie/sharingmap/designsystem/theme/Color.kt
@@ -14,5 +14,6 @@ val Gray9A9C9F = Color(0xFF9A9C9F)
 val GrayA8AAAB = Color(0xFFA8AAAB)
 val GrayE6E6E6 = Color(0xFFE6E6E6)
 val GrayE8E9EA = Color(0xFFE8E9EA)
+val GrayD9D9D9 = Color(0xFFD9D9D9)
 
 val GrayE8EAEB_30 = Color(0x40E8EAEB)
