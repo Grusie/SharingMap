b10b0b84fafc38317b45ecea16c93da9c49307d2
Choisehyeon <chltpgus6275@gmail.com>
2024-09-08 14:57:14 +0900

    feat: 스크린 이동 시 바텀 네비게이션 바를 숨겨야하는 스크린 정리

--- app/src/main/java/com/grusie/sharingmap/ui/main/MainActivity.kt
+++ app/src/main/java/com/grusie/sharingmap/ui/main/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.grusie.sharingmap.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.grusie.sharingmap.ui.navigation.main.MainBottomNavGraph
 import com.grusie.sharingmap.ui.navigation.main.MainBottomNavItem
 import com.grusie.sharingmap.ui.navigation.main.MainBottomNavigationBar
 import com.grusie.sharingmap.designsystem.theme.SharingMapTheme
+import com.grusie.sharingmap.ui.main.mypage.storage.StorageScreen
+import com.grusie.sharingmap.ui.navigation.main.NavItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,22 +27,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination?.route
+            var isBottomBarVisible = when {
+                currentDestination?.startsWith(NavItem.Storage.screenRoute) == true -> false
+                else -> true
+            }
             SharingMapTheme {
                 Scaffold(
                     bottomBar = {
-                        MainBottomNavigationBar(
-                            items = listOf(
-                                MainBottomNavItem.Home,
-                                MainBottomNavItem.Map,
-                                MainBottomNavItem.Edit,
-                                MainBottomNavItem.Search,
-                                MainBottomNavItem.MyPage
-                            ), navController = navController
-                        )
+                        if(isBottomBarVisible) {
+                            MainBottomNavigationBar(
+                                items = listOf(
+                                    MainBottomNavItem.Home,
+                                    MainBottomNavItem.Map,
+                                    MainBottomNavItem.Edit,
+                                    MainBottomNavItem.Search,
+                                    MainBottomNavItem.MyPage
+                                ), navController = navController
+                            )
+                        }
                     }
                 ) {
                     Box(Modifier.padding(it)) {
-                        MainBottomNavGraph(navController) { finish() }
+                        MainBottomNavGraph(navController = navController) { finish() }
                     }
                 }
             }
