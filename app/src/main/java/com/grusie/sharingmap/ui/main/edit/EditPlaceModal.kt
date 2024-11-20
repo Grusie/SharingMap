2fa5556a8c71f429e56187fb85391f0c207561bc
Grusie <rbtjr02@naver.com>
2024-11-20 21:39:08 +0900

    feat: 갤러리 이미지 넣기 임시 구현

--- app/src/main/java/com/grusie/sharingmap/ui/main/edit/EditPlaceModal.kt
+++ app/src/main/java/com/grusie/sharingmap/ui/main/edit/EditPlaceModal.kt
@@ -1,5 +1,8 @@
 package com.grusie.sharingmap.ui.main.edit
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -9,12 +12,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text2.BasicTextField2
@@ -23,6 +30,8 @@ import androidx.compose.foundation.text2.input.TextFieldState
 import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
@@ -32,8 +41,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,12 +57,14 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.grusie.sharingmap.R
 import com.grusie.sharingmap.designsystem.component.CustomBottomSheetDragHandle
 import com.grusie.sharingmap.designsystem.theme.Black
 import com.grusie.sharingmap.designsystem.theme.Blue458FFF
 import com.grusie.sharingmap.designsystem.theme.Gray9A9C9F
 import com.grusie.sharingmap.designsystem.theme.GrayE6E6E6
+import com.grusie.sharingmap.designsystem.theme.GrayE8EAEB_30
 import com.grusie.sharingmap.designsystem.theme.GrayF1F4F7
 import com.grusie.sharingmap.designsystem.theme.Typography
 import com.grusie.sharingmap.designsystem.theme.White
@@ -109,7 +125,21 @@ fun EditPlaceBottomSheetContent(
     val placeTextFieldState = rememberTextFieldState(additionalArchiveModel.placeName)
     val contentTextFieldState = rememberTextFieldState()
     val scrollState = rememberScrollState()
+    val attachList by remember { mutableStateOf(ArrayList<AttachUiModel>()) }
 
+    val pickMultipleMedia =
+        rememberLauncherForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            if (uris.isNotEmpty()) {
+                uris.forEach {
+                    attachList.add(
+                        AttachUiModel(
+                            id = attachList.size.toLong(),
+                            src = it.toString()
+                        )
+                    )
+                }
+            }
+        }
 
     Column()
     {
@@ -183,6 +213,7 @@ fun EditPlaceBottomSheetContent(
             HorizontalDivider(color = GrayE6E6E6)
 
             CustomTextField(
+                modifier = Modifier.padding(12.dp),
                 textFieldState = placeTextFieldState,
                 hintText = stringResource(id = R.string.edit_place_hint)
             )
@@ -198,17 +229,31 @@ fun EditPlaceBottomSheetContent(
 
             HorizontalDivider(color = GrayE6E6E6)
 
-            CustomTextField(
-                textFieldState = contentTextFieldState,
-                hintText = stringResource(id = R.string.edit_content_hint),
-                keyboardOptions = KeyboardOptions.Default,
-                lineLimits = TextFieldLineLimits.MultiLine(7),
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                CustomTextField(
+                    textFieldState = contentTextFieldState,
+                    hintText = stringResource(id = R.string.edit_content_hint),
+                    keyboardOptions = KeyboardOptions.Default,
+                    lineLimits = TextFieldLineLimits.MultiLine(7),
+                )
+
+                AttachListView(
+                    attachList
+                ) {
+                    attachList.remove(it)
+                }
+            }
 
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
                 IconButton(
                     modifier = Modifier.size(24.dp),
-                    onClick = {}
+                    onClick = {
+                        pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_gallery),
@@ -242,10 +287,9 @@ fun EditPlaceBottomSheetContent(
 fun CustomTextField(
     textFieldState: TextFieldState,
     hintText: String,
-    modifier: Modifier = Modifier.fillMaxWidth(),
+    modifier: Modifier = Modifier,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
     lineLimits: TextFieldLineLimits = TextFieldLineLimits.SingleLine,
-    attachList: List<AttachUiModel> = emptyList()
 ) {
     BasicTextField2(
         state = textFieldState,
@@ -255,33 +299,78 @@ fun CustomTextField(
             lineHeight = TextUnit(21f, TextUnitType.Sp)
         ),
         lineLimits = lineLimits,
-        modifier = modifier,
+        modifier = modifier.fillMaxWidth(),
         decorator = { innerTextField ->
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                if (textFieldState.text.isEmpty()) {
-                    Text(
-                        text = hintText,
-                        style = Typography.headlineSmall,
-                        color = Gray9A9C9F,
-                    )
-                }
-                innerTextField()
+
+            if (textFieldState.text.isEmpty()) {
+                Text(
+                    text = hintText,
+                    style = Typography.headlineSmall,
+                    color = Gray9A9C9F,
+                )
             }
+            innerTextField()
         }
     )
+}
 
+@Composable
+fun AttachListView(
+    attachList: List<AttachUiModel> = emptyList(),
+    removeAttachItem: (AttachUiModel) -> Unit
+) {
     if (attachList.isNotEmpty())
         LazyRow(
             modifier = Modifier
                 .padding(top = 12.dp)
                 .fillMaxWidth()
         ) {
-
+            items(attachList) {
+                AttachView(it, removeAttachItem)
+            }
         }
+}
+
+@Composable
+fun AttachView(
+    attachUiModel: AttachUiModel,
+    removeAttachItem: (AttachUiModel) -> Unit = {}
+) {
+    Row() {
+        Box(
+            modifier = Modifier
+                .width(153.dp)
+                .height(122.dp)
+                .background(shape = RoundedCornerShape(12.dp), color = Black)
+        ) {
+            AsyncImage(
+                model = attachUiModel.src,
+                contentDescription = "attach_item",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(12.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            IconButton(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(6.dp)
+                    .background(shape = CircleShape, color = GrayE8EAEB_30)
+                    .size(24.dp)
+                    .padding(2.dp),
+                onClick = {
+                    removeAttachItem(attachUiModel)
+                }) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    tint = White,
+                    contentDescription = "ic_image_remove"
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+    }
 }
 
 
@@ -289,4 +378,10 @@ fun CustomTextField(
 @Preview(showBackground = true)
 fun EditPlaceBottomSheetPreview() {
     EditPlaceBottomSheetContent(additionalArchiveModel = AdditionalArchiveModel(address = "서울특별시 서초구 효령로49길 52"))
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AttachViewPreview() {
+    AttachView(AttachUiModel(id = 0, src = ""))
 }
--- app/src/main/java/com/grusie/sharingmap/ui/model/AttachUiModel.kt
+++ app/src/main/java/com/grusie/sharingmap/ui/model/AttachUiModel.kt
@@ -1,6 +1,6 @@
 package com.grusie.sharingmap.ui.model
 
 data class AttachUiModel(
-    val src: String,
-    val id: Long,
+    val src: String = "",
+    val id: Long = 0L,
 )
